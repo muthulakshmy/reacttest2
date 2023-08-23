e8ee1b0bd30e51824ddda28c2914387e2b66b198
Muthulakshmy <muthulakshmy14072002@gmail.com>
2023-08-23 18:48:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+function Library() {
+  const [books, setBooks] = useState([])
+  const [Count, setCount] = useState(0)
+  const [bookTitle, setBookTitle] = useState(' ')
+  const [author, setAuthor] = useState(' ')
+
+  const Add = (name, author) => {
+    const newBook = { name, author, read: false }
+    setBooks([...books, newBook])
+  };
+
+//  const myDiv={
+//   display:"flex",
+//   flexDirection:"column",
+
+//  }
+  const Read = (id) => {
+    const readedBooks = books.map((book, i) => {
+      if (i === id) {
+        const readBook = { ...book, read: true ,disabled: true}
+        if (readBook.read) {
+          setCount(Count + 1)
+        }
+       
+        return readBook
+      }
+      return book
+    })
+    setBooks(readedBooks)
+  }
+  
+  const Delete = (id) => {
+    const book = books.filter((value,id1) => id1 !== id)
+    console.log(book)
+    setBooks(book)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div >
+      <h1>Library</h1>
+     
+      <div className='myLibrary'>
+        <label>
+          Book Title : <br/>
+        <input placeholder="Book Title" value={bookTitle} onChange={(e) => setBookTitle(e.target.value)} />
+        </label>
+        <label>
+        Book Author : <br/>
+        <input placeholder="Book Author" value={author} onChange={(e) => setAuthor(e.target.value)}/><br/>
+        <button onClick={() => Add(bookTitle, author)}>Add Book</button>
+        </label>
+
+      </div>
+      <div>
+      <p>Books finished : {Count}</p>
+      </div>
+      <div>
+        {books.map((book, id) => (
+          <div >
+            <p style={{color:book.read ? 'green' : 'red'}} >
+             Title :  {book.name} <br/> Author : {book.author}
+            </p>
+            <button onClick={() => Read(id)} disabled={book.disabled}>
+              {book.read ? 'Book Finished' : 'Read' }
+            </button>
+            <button className ="delete" onClick={() => Delete(id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
+    </body>
   );
 }
 
-export default App;
+export default Library
+
